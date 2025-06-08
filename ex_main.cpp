f096dadbf25ea752f2cf8407dc5531d36faf53a9
23060128-王桑平 <3361833374@qq.com>
2025-06-08 09:41:38 +0800

    add description

--- ex_main.cpp
+++
@@ -1,6 +0,0 @@
-// #include "Include/Monitor.hpp"
-
-// int main(int argc, char *argv[]){
-//     Monitor * MonitorObj = new Monitor(argc,argv);
-//     return 0;//MonitorObj->is_exit_status_bad();
-// }
